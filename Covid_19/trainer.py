9dac54420fa53b0181d9bc4eacc0c550370bde93
Nina Levar <nina.l.ams@gmail.com>
2021-06-16 10:27:34 +0200

    updated trainer.py file

--- Covid_19/trainer.py
+++ Covid_19/trainer.py
@@ -10,12 +10,18 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras import optimizers
 from tensorflow.keras.callbacks import EarlyStopping
 
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping
+
 from os.path import join, dirname
 import dotenv
 from dotenv import load_dotenv
 
 import os
 import glob
+
+from PIL import Image
+
 
 
 # ...
@@ -85,7 +91,7 @@ def get_data(label):
 
         images = []
         
-        # for blob in blobs[:10]:
+        # for blob in blobs:
         #     #print(blob.name)
         #     if blob.name.endswith(".jpg"):
         #         image = cv2.imdecode(np.asarray(bytearray(blob.download_as_string())), 0)
@@ -95,12 +101,16 @@ def get_data(label):
         ct = 0
     
         for blob in blobs:
-            while ct < 50:
+            while ct < 2500:
                 #print(blob.name)
                 if blob.name.endswith(".jpg"):
+                    #blob.download_to_filename('downloaded_image.png')
                     image = cv2.imdecode(np.asarray(bytearray(blob.download_as_string())), 0)
+                    #image = Image.open("downloaded_image.png") #.convert('RGB')
                     #print(image.shape)         
-                    images.append(image)  
+                    images.append(image)
+                    #images.append(np.array(image))
+
                 ct +=1
         
         return images
@@ -155,10 +165,54 @@ def compile_model(model):
                   metrics=['accuracy'])
     return model      
     
-def train_model(X_train, y_train):
+def data_augmentation_model(X_train, y_train, X_test, y_test, model):
+    datagen = ImageDataGenerator(
+        featurewise_center=False,
+        featurewise_std_normalization=False,
+        rotation_range=10,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        horizontal_flip=False,
+        vertical_flip=False,
+        zoom_range=(0.8, 1.2),
+        )
+    
+    datagen.fit(X_train)
+    
+    X_augmented_iterator = datagen.flow(X_train, shuffle=False, batch_size=1)
+    
+    # The model
+    model_aug = initialize_model()
+    model_aug = compile_model(model_aug)
+    
+    # The data generator
+    X_tr = X_train[:40000]
+    y_tr = y_train[:40000]
+    X_val = X_train[40000:]
+    y_val = y_train[40000:]
+    train_flow = datagen.flow(X_tr, y_tr, batch_size=64)
+    
+    # The early stopping criterion
+    es = EarlyStopping(patience=3)
+    
+    # The fit
+    history_aug = model_aug.fit(train_flow,
+                            epochs=50, 
+                            callbacks=[es], 
+                            validation_data=(X_val, y_val))
+    
+    #res_1 = model.evaluate(X_test, y_test, verbose=0)
+    res_2 = model_aug.evaluate(X_test, y_test)
+    
+    #print(f'Accuracy without data augmentation {res_1[1]*100:.2f}%')
+    print(f'Accuracy with data augmentation {res_2[1]*100:.2f}%')
+    
+    return model_aug      
+    
+def train_model(model, X_train, y_train):
     """method that trains the model"""
     print('Training data')
-    es = EarlyStopping(patience=50, verbose=2)
+    es = EarlyStopping(patience=5, verbose=2)
     history = model.fit(X_train, y_train, 
                 validation_split=0.3,
                 callbacks=[es], 
@@ -183,7 +237,7 @@ def upload_model_to_gcp():
     # #blob.upload_from_filename('model.joblib')
     # blob.upload_from_filename('model')
     
-    upload_from_directory('model', BUCKET_NAME, 'model_test')
+    upload_from_directory('model_2500', BUCKET_NAME, 'model_2500')  #creates model_complete under Covid-19 , then create models
     
 def upload_from_directory(directory_path: str, dest_bucket_name: str, dest_blob_name: str):
     client = storage.Client()
@@ -195,16 +249,14 @@ def upload_from_directory(directory_path: str, dest_bucket_name: str, dest_blob_
             blob = bucket.blob(remote_path)
             blob.upload_from_filename(local_file)
 
-
-
 def save_model(reg):
     """method that saves the model into a .joblib file and uploads it on Google Storage /models folder
     HINTS : use joblib library and google-cloud-storage"""
     
     # saving the trained model to disk is mandatory to then beeing able to upload it to storage
     # Implement here
-    
-    model.save('model')  # creates a HDF5 file 'my_model.h5'
+    #model.save('model_2000')
+    model.save('./model_2500/model_2500.h5')  # creates a HDF5 file 'my_model.h5' locally
     #joblib.dump(reg, 'model.joblib')
     #print("saved model.joblib locally")
 
@@ -224,30 +276,42 @@ if __name__ == '__main__':
     
     
     X_nCT = get_data('nCT')
+    print(len(X_nCT))
     X_nCT = np.expand_dims(X_nCT, axis=-1)
+    print(len(X_nCT))
     X_pCT = get_data('pCT')
+    print(len(X_pCT))
     X_pCT = np.expand_dims(X_pCT, axis=-1)
+    print(len(X_pCT))
     X_NiCT = get_data('NiCT')
+    print(len(X_NiCT))
     X_NiCT = np.expand_dims(X_NiCT, axis=-1)
-    
+    print(len(X_NiCT))
+
 
     # Compute labels
     labels_nCT = compute_labels(0, (np.shape(X_nCT)[0], 1), int)
     labels_pCT = compute_labels(1, (np.shape(X_pCT)[0], 1), int) 
-    labels_NiCT = compute_labels(2, (np.shape(X_NiCT)[0], 1), int) 
-
+    labels_NiCT = compute_labels(2, (np.shape(X_NiCT)[0], 1), int)
+    
+    print(labels_nCT.shape, labels_pCT.shape, labels_NiCT.shape)
+    
     # preprocess data
     X_nCT_train, X_nCT_test, y_nCT_train, y_nCT_test = preprocess(X_nCT, labels_nCT)
     X_pCT_train, X_pCT_test, y_pCT_train, y_pCT_test = preprocess(X_pCT, labels_pCT)
     X_NiCT_train, X_NiCT_test, y_NiCT_train, y_NiCT_test = preprocess(X_NiCT, labels_NiCT)
     
-    print(X_nCT_train.shape, X_nCT_test.shape, X_pCT_train.shape,X_pCT_test.shape, X_NiCT_train.shape, X_NiCT_test.shape)
+    print(X_nCT_train.shape, X_nCT_test.shape, X_pCT_train.shape, X_pCT_test.shape, X_NiCT_train.shape, X_NiCT_test.shape)
 
     # Stack pCT, nCT, and NiCT them
     X_train = np.concatenate((X_nCT_train, X_pCT_train, X_NiCT_train), axis=0)
     print(f'Dimensions of X_train: {np.shape(X_train)}')
     y_train = np.concatenate((y_nCT_train, y_pCT_train, y_NiCT_train), axis=0)
     print(f'Dimensions of y_train: {np.shape(y_train)}')
+
+    # augmentation
+    #data_augmentation_model(    )
+
 
     # Initialize model
     print('initializing model')
@@ -261,7 +325,7 @@ if __name__ == '__main__':
     # or on GCP if it was called through the gcp_submit_training, in which case
     # this package is uploaded to GCP before being executed)
     print('train model')
-    reg = train_model(X_train, y_train)
+    reg = train_model(model, X_train, y_train)
 
     # Evaluate model
 
